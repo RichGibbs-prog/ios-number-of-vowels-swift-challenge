8367c98db13f39af17b46f6e919dc6a5723cf45d
Rich Gibbs <rich-gibbs@lambdastudents.com>
2020-05-13 08:54:46 -0700

    finished the code challenge @mrflowers22

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -4,22 +4,25 @@ var count = 0
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
     for letterCount in string {
         switch letterCount {
-        case "A", "E", "I", "O", "U", "a", "e", "i", "o", "u":
+        case "A", "E", "I", "O", "U", "Y", "a", "e", "i", "o", "u", "y":
             count += 1
-       
-        
-        
-        
-        default:
+            if isYAVowel == false {
+            switch letterCount {
+            case "y", "Y":
+            count -= 1
+            default:
             break
         }
+            }
+        default:
+            continue
     }
-    return count
+   
 }
 
-
-
+ return count
+}
 numberOfVowels(in: "Polly wants A cracker", isYAVowel: true)
-
+numberOfVowels(in: "I love learning how to code one day I will have a nice stable career to take care of my daugher.", isYAVowel: false)
 
 
