05bcff2980d28be28ba267409f560132573ba01b
Rich Gibbs <rich-gibbs@lambdastudents.com>
2020-05-13 08:42:16 -0700

    got the funtion to count the vowels @mrflowers22

--- numberOfVowels.playground/Contents.swift
+++ numberOfVowels.playground/Contents.swift
@@ -1,5 +1,25 @@
 import UIKit
+var count = 0
 
 func numberOfVowels(in string: String, isYAVowel: Bool = false) -> Int {
-    
+    for letterCount in string {
+        switch letterCount {
+        case "A", "E", "I", "O", "U", "a", "e", "i", "o", "u":
+            count += 1
+       
+        
+        
+        
+        default:
+            break
+        }
+    }
+    return count
 }
+
+
+
+numberOfVowels(in: "Polly wants A cracker", isYAVowel: true)
+
+
+
